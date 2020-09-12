1b57e5a6782b8953f1923d4c51adc8e32ff237fb
Reggie Littleton <Hitsupod@gmail.com>
2020-09-11 21:43:26 -0500

    Components Creation

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+/* 
+Add the following Express routes for your app:
+
+* `/api/books` (get) - Should return all saved books as JSON.
+
+* `/api/books` (post) - Will be used to save a new book to the database.
+
+* `/api/books/:id` (delete) - Will be used to delete a book from the database by Mongo `_id`.
+*/
